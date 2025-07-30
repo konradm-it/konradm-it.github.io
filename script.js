21d32ef456ea050a294a7a2b1208d17288dcbde0
konradm-it <konradmichalak007@gmail.com>
2025-07-30 11:43:33 +0200

    optymalizacja pod telefon cz2

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    document.querySelector('.menu').classList.toggle('active'); 
+}
+
+function hideMenu() {
+    document.querySelector('.menu').classList.remove('active');
+}
